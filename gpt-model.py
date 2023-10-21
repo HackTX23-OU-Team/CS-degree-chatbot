5d38dafafd6ae076f1c1a782091ca49ee388af01
Kristiana Karshelieva <kris.krsh02@gmail.com>
2023-10-21 17:19:47 -0500

    Create basic LlamaIndex Scripts

---
+++ gpt-model.py
@@ -0,0 +1,27 @@
+import streamlit as st
+from llama_index import VectorStoreIndex, ServiceContext, Document
+from llama_index.llms import OpenAI
+import openai
+from llama_index import SimpleDirectoryReader
+
+openai.api_key = st.secrets.openai_key
+st.title("The Ultimate OU Advisor for CS majors")
+
+if "messages" in st.session_state.keys():
+    st.session_state.messages = [
+        {"role": "assistant", "content": "Ask me a question for your CS Flowchart"}
+    ]
+
+
+@st.cache_resource(show_spinner=False)
+def load_data():
+    with st.spinner(text="Loading and indexing the Streamlit docs – hang tight! This should take 1-2 minutes."):
+        reader = SimpleDirectoryReader(input_dir="./data", recursive=True)
+        docs = reader.load_data()
+        service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt="This is a trial chatbot for CS students from the The University of Oklahoma. You are an expert at helping CS students navigate their flowchart."))
+        index = VectorStoreIndex.from_documents(docs, service_context=service_context)
+        return index
+
+index = load_data()
+
+chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True)
